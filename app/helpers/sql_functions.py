f02a054cf95e1d44e0f5f8a24e5d116a2147bdcd
arynor96 <bg.adrian96@gmail.com>
2022-06-14 01:18:37 +0200

    added SQL function for delete/create the all table

---
+++ app/helpers/sql_functions.py
@@ -0,0 +1,89 @@
+def delete_sql_tables(db):
+    cursor = db.cursor()
+    # do not change the order, constraints problem may happen due to foreign key
+    cursor.execute('''DROP TABLE IF EXISTS Likes''')
+    cursor.execute('''DROP TABLE IF EXISTS Review''')
+    cursor.execute('''DROP TABLE IF EXISTS Song''')
+    cursor.execute('''DROP TABLE IF EXISTS Album''')
+    cursor.execute('''DROP TABLE IF EXISTS Artist''')
+    cursor.execute('''DROP TABLE IF EXISTS Users''')
+    cursor.close()
+    return True
+
+
+def create_sql_tables(db):
+    cursor = db.cursor()
+
+    sql_statement = '''
+    
+            CREATE TABLE Artist (
+                artist_id INTEGER NOT NULL AUTO_INCREMENT,
+                artist_name VARCHAR(50) NOT NULL,
+                artist_genre VARCHAR(50),
+                PRIMARY KEY (artist_id)
+            );
+    
+            CREATE TABLE Album (
+                album_id INTEGER NOT NULL AUTO_INCREMENT,
+                album_name VARCHAR(50) NOT NULL,
+                album_release_date DATE,
+                artist_id INTEGER NOT NULL,
+                PRIMARY KEY (album_id),
+                FOREIGN KEY (artist_id) REFERENCES Artist(artist_id) ON DELETE CASCADE
+            );
+            
+            CREATE Table Users(
+                email VARCHAR(50) NOT NULL,
+                username VARCHAR(50) NOT NULL,
+                password VARCHAR(50) NOT NULL,
+                user_registration_date DATE,
+                followed_by VARCHAR(50),
+                PRIMARY KEY (email),
+                FOREIGN KEY (followed_by) REFERENCES Users(email)
+            );
+            
+            CREATE TABLE Song(
+                song_id INTEGER NOT NULL AUTO_INCREMENT,
+                song_title VARCHAR(50) NOT NULL,
+                song_length INTEGER,
+                song_release_date DATE,
+                album_id INTEGER,
+                PRIMARY KEY (song_id),
+                FOREIGN KEY (album_id) REFERENCES Album(album_id) ON DELETE CASCADE                
+            );
+            
+            CREATE TABLE Review(
+                 album_id INTEGER,
+                 email VARCHAR(50),
+                 text VARCHAR(200),
+                 review_rating INTEGER NOT NULL,
+                 review_date DATE NOT NULL,
+                 CHECK (review_rating<=5),
+                 CHECK (review_rating>=0),
+                 FOREIGN KEY (album_id) REFERENCES Album(album_id) ON DELETE CASCADE,
+                 FOREIGN KEY (email) REFERENCES Users(email),
+                 PRIMARY KEY (album_id,email)  
+            );
+            
+            CREATE TABLE Likes(
+                song_id INTEGER,
+                email VARCHAR(50),
+                FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE,
+                FOREIGN KEY (song_id) REFERENCES Song(song_id) ON DELETE CASCADE,
+                PRIMARY KEY (song_id,email)
+            );
+            
+            
+            
+           
+    
+    '''
+
+    # source: https://pythonexamples.org/python-iterate-over-words-of-string/
+    for part in sql_statement.split(';'):
+        if len(part.strip()) > 0:
+            cursor.execute(part)
+
+    db.commit()
+    cursor.close()
+    return True
--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import pymongo
 from flask import Flask
 import mysql.connector
 
+from app.helpers.sql_functions import create_sql_tables, delete_sql_tables
+
 app = Flask(__name__)
 
 sql_config = {'user': 'user', 'password': 'password', 'host': 'sql', 'port': '3306', 'database': 'imse_m2_db'}
 db = mysql.connector.connect(**sql_config)
-
 
 mongo_client = pymongo.MongoClient('mongodb://user:password@mongo:27017')
 mongo_db = mongo_client['imse_m2_mongo']
@@ -15,3 +16,13 @@ mongo_db = mongo_client['imse_m2_mongo']
 @app.route('/')
 def hello_world():
     return 'Flask, MySQL, MongoDB and Docker are running'
+
+
+@app.route('/delete_db')
+def delete_db():
+    if delete_sql_tables(db): return 'Database is empty now'
+
+
+@app.route('/create_db')
+def create_db():
+    if create_sql_tables(db): return 'Database tables created'
