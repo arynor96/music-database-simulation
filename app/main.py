dd39bcd0ec7108d94f3e42087f193daeda0f2987
arynor96 <bg.adrian96@gmail.com>
2022-06-15 17:46:53 +0200

    added bootstrap, base and initialization/reset method

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import pymongo
-from flask import Flask
+from flask import Flask, render_template, redirect
 import mysql.connector
 
 from app.helpers.sql_functions import create_sql_tables, delete_sql_tables, fill_sql_tables
 
 app = Flask(__name__)
+app.config['DB_STATUS'] = ''
+app.config['SECRET_KEY'] = 'ERROR'
 
 sql_config = {'user': 'user', 'password': 'password', 'host': 'sql', 'port': '3306', 'database': 'imse_m2_db'}
 db = mysql.connector.connect(**sql_config)
@@ -15,7 +17,7 @@ mongo_db = mongo_client['imse_m2_mongo']
 
 @app.route('/')
 def hello_world():
-    return 'Flask, MySQL, MongoDB and Docker are running'
+    return render_template("index.html")
 
 
 @app.route('/delete_db')
@@ -33,8 +35,22 @@ def fill_db():
     fill_sql_tables(db)
 
 
+# if database is already initialized then it will return to home page
+# TODO: when we have a proper website -> a proper redirect and warning message
 @app.route('/initialize')
 def initialize_db():
+    if not app.config['DB_STATUS']:
+        app.config['DB_STATUS'] = 'SQL'
+        delete_sql_tables(db)
+        create_sql_tables(db)
+        if fill_sql_tables(db): return 'Database initialization done'
+    else:
+        return redirect('/')
+
+
+
+@app.route('/reset')
+def reset_db():
     delete_sql_tables(db)
     create_sql_tables(db)
-    if fill_sql_tables(db): return 'Database initialization done'
+    if fill_sql_tables(db): return 'Database reset done'
