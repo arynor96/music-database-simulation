188627d0453bbed4525a623ad5dbd4b2778e7d26
arynor96 <bg.adrian96@gmail.com>
2022-06-18 03:09:38 +0200

    post a review, first initialize before doing anything

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pymongo
 from flask import Flask, render_template, redirect, session, flash, request, url_for
 import mysql.connector
@@ -27,9 +29,13 @@ def home():
     else:
         return render_template("index.html")
 
+
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    msg = ''
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     if request.method == "POST" and 'email' in request.form and 'pass' in request.form:
         session.permanent = True
         user = request.form['email']
@@ -37,12 +43,12 @@ def login():
         cursor.execute('SELECT * FROM Users WHERE email = %s AND password = %s', (user, user_password,))
         account = cursor.fetchone()
         if account:
-           session["user"] = user
-           flash("Login succesful!")
-           return redirect(url_for("home"))
+            session["user"] = user
+            flash("Login succesful!")
+            return redirect(url_for("home"))
         else:
-           flash("Incorrect email/password!")
-           return render_template("login.html")
+            flash("Incorrect email/password!")
+            return render_template("login.html")
     else:
         if "user" in session:
             flash("Already logged in!")
@@ -52,13 +58,18 @@ def login():
 
 @app.route("/logout")
 def logout():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     if "user" in session:
         user = session["user"]
         flash(f"You have been logged out, {user}", "info")
     session.pop("user", None)
-    return redirect(url_for("login"))
+    return redirect(url_for("home"))
 
-@app.route("/register", methods=["POST","GET"])
+
+@app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == 'POST' and 'email' in request.form and 'username' in request.form and 'pass' in request.form:
         email = request.form['email']
@@ -76,15 +87,21 @@ def register():
             flash("Please fill out the form!")
         else:
             current_date = datetime.date.today()
-            cursor.execute('INSERT INTO Users VALUES (%s, %s, %s, %s)', (email, username, user_password, current_date.strftime('%Y-%m-%d %H:%M:%S'),))
+            cursor.execute('INSERT INTO Users VALUES (%s, %s, %s, %s)',
+                           (email, username, user_password, current_date.strftime('%Y-%m-%d %H:%M:%S'),))
             db.commit()
             flash("You have successfully registered!")
-    elif request.method ==  'POST':
+    elif request.method == 'POST':
         flash('Please fill out the form!')
     return render_template("register.html")
 
+
 @app.route('/albums')
 def albums():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute("SELECT * FROM Album")
     results = cursor.fetchall()
     return render_template("albums.html", data=results)
@@ -92,6 +109,10 @@ def albums():
 
 @app.route('/artists')
 def artists():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute("SELECT * FROM Artist")
     results = cursor.fetchall()
     return render_template("artists.html", data=results)
@@ -99,6 +120,10 @@ def artists():
 
 @app.route('/songs')
 def songs():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute("SELECT * FROM Song")
     results = cursor.fetchall()
     return render_template("songs.html", data=results)
@@ -120,6 +145,10 @@ def follows():
 
 @app.route('/reviews')
 def reviews():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute("SELECT * FROM Review")
     results = cursor.fetchall()
     return render_template("reviews.html", data=results)
@@ -127,6 +156,10 @@ def reviews():
 
 @app.route('/topalbums')
 def topalbums():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute(
         "SELECT artist_name, album_name, averageRating FROM (SELECT Review.album_id, Artist.artist_id, Artist.artist_name, Album.album_name, avg(review_rating) As averageRating FROM Review  LEFT JOIN Album ON Album.album_id = Review.album_id LEFT JOIN Artist ON Artist.artist_id = Album.artist_id GROUP BY Artist.artist_id, Review.album_id) a WHERE NOT EXISTS (SELECT * FROM (SELECT Review.album_id, Artist.artist_id, Artist.artist_name,  avg(review_rating) As averageRating FROM Review LEFT JOIN Album ON Album.album_id = Review.album_id LEFT JOIN Artist ON Artist.artist_id = Album.artist_id GROUP BY Artist.artist_id, Review.album_id) b WHERE a.artist_id = b.artist_id AND a.averageRating < b.averageRating ) ORDER BY averageRating DESC")
     results = cursor.fetchall()
@@ -135,6 +168,10 @@ def topalbums():
 
 @app.route('/mostreviews')
 def mostreviews():
+    if app.config['DB_STATUS'] == '':
+        flash("Please initialize database first!")
+        return render_template("init.html")
+
     cursor.execute(
         "SELECT Album.album_name, COUNT(Review.album_id) as review_count FROM Album LEFT JOIN Review ON Album.album_id = Review.album_id LEFT JOIN Users ON Review.email = Users.email WHERE YEAR(Users.user_registration_date) = YEAR(NOW()) - 1 GROUP BY Album.album_name ORDER BY review_count DESC")
     results = cursor.fetchall()
@@ -176,3 +213,60 @@ def reset_db():
     create_sql_tables(db)
     app.config['DB_STATUS'] = 'SQL'
     if fill_sql_tables(db): return render_template("index.html")
+
+
+# if a user has already reviewed an album, the review will be updated and the user will be notified
+@app.route('/review_add', methods=["POST", "GET"])
+def review_add():
+    try: session["user"]
+    except:
+        flash("Please login first!")
+        return render_template("index.html")
+
+    if request.method == "POST" and 'album' in request.form and 'rating' in request.form and 'review' in request.form:
+        name = request.form['album']
+        if name == '':
+            flash("Please add an album name")
+            return render_template("review_add.html")
+        rating = request.form['rating']
+
+        if rating == '':
+            flash("Please add a rating")
+            return render_template("review_add.html")
+        else:
+            rating = int(request.form['rating'])
+
+        review = request.form['review']
+        cursor.execute('SELECT album_id FROM Album WHERE album_name LIKE %s', (name,))
+        album = cursor.fetchone()
+        if album is None:
+            flash("Incorrect Album")
+            return render_template("review_add.html")
+        if rating < 1 or rating > 5:
+            flash("Rating must be between 1 and 5")
+            return render_template("review_add.html")
+        if review is None:
+            review = ''
+        for row in album:
+            albumid = row
+
+        print(albumid, file=sys.stderr)
+        current_date = datetime.date.today()
+        try:
+            cursor.execute('INSERT INTO Review VALUES (%s, %s, %s, %s, %s)',
+                           (albumid, session["user"], review, rating, current_date.strftime('%Y-%m-%d %H:%M:%S')))
+        except:
+            flash("Review on album updated")
+            email_current = session["user"]
+            cursor.execute('DELETE FROM  Review WHERE album_id = %s and email = %s', (albumid, email_current))
+            cursor.execute('INSERT INTO Review VALUES (%s, %s, %s, %s, %s)',
+                           (albumid, session["user"], review, rating, current_date.strftime('%Y-%m-%d %H:%M:%S')))
+            db.commit()
+            return render_template("review_add.html")
+        db.commit()
+        flash("Review successfully submitted!")
+
+    elif request.method == 'POST':
+        flash('Please fill out the form!')
+
+    return render_template("review_add.html")
